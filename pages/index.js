8045fda9cdf47b04229f42609c62bec5adb217b4
MGexperiences <info@mg-experiences.com>
2026-04-22 16:42:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import fs from 'fs'
-import path from 'path'
-
-export default function Home({ html }) {
-  return <div dangerouslySetInnerHTML={{ __html: html }} />
+export default function Home() {
+  return null
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({ res }) {
+  const fs = require('fs')
+  const path = require('path')
   const html = fs.readFileSync(path.join(process.cwd(), 'public/index.html'), 'utf8')
-  const body = html.match(/<body[^>]*>([\s\S]*)<\/body>/i)?.[1] || ''
-  return { props: { html: body } }
+  res.setHeader('Content-Type', 'text/html')
+  res.write(html)
+  res.end()
+  return { props: {} }
 }

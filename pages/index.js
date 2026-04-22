5553817181b02ea14d0f512a0d9169e26739d305
MGexperiences <info@mg-experiences.com>
2026-04-22 16:40:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import { useEffect } from 'react'
+import fs from 'fs'
+import path from 'path'
 
-export default function Home() {
-  useEffect(() => {
-    window.location.href = '/index.html'
-  }, [])
-  return null
+export default function Home({ html }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
+}
+
+export async function getStaticProps() {
+  const html = fs.readFileSync(path.join(process.cwd(), 'public/index.html'), 'utf8')
+  const body = html.match(/<body[^>]*>([\s\S]*)<\/body>/i)?.[1] || ''
+  return { props: { html: body } }
 }
